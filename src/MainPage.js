0a9b83ad6d609b8118d87e6129014b3e93d9a9c2
izasa <iza.saltarius@gmail.com>
2023-01-19 13:20:17 +0100

    initial commit

---
+++ src/MainPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainPage extends Component {
+    render() {
+        return <div>
+            Hi, this is Main Page
+            <h5>Helllo</h5>
+        </div>
+
+    }
+}
+
+export default MainPage;
---
+++ src/OtherPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class OtherPage extends Component {
+    render() {
+        return <div>
+            Hi, this is OtherPage!
+        </div>
+
+    }
+}
+
+export default OtherPage;
